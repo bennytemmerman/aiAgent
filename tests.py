7be4ea4677acd57674987c9cae4e251492ec9d9d
bennytemmerman <81377775+bennytemmerman@users.noreply.github.com>
2025-06-11 16:49:55 +0200

    Update tests.py

--- tests.py
+++ tests.py
@@ -1,4 +1,5 @@
 from functions.get_files_info import get_files_info
+from functions.get_file_content import get_file_content
 
 if __name__ == "__main__":
     print("Test 1: get_files_info('calculator', '.')")
@@ -12,3 +13,12 @@ if __name__ == "__main__":
 
     print("Test 4: get_files_info('calculator', '../')")
     print(get_files_info("calculator", "../"), end="\n\n")
+
+    print("Test 1: get_file_content('calculator', 'main.py')")
+    print(get_file_content("calculator", "main.py"), end="\n\n")
+
+    print("Test 2: get_file_content('calculator', 'pkg/calculator.py')")
+    print(get_file_content("calculator", "pkg/calculator.py"), end="\n\n")
+
+    print("Test 3: get_file_content('calculator', '/bin/cat')")
+    print(get_file_content("calculator", "/bin/cat"), end="\n\n")
