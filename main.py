69242be0e95f0100481fb9c3a9b61e54daceccad
bennytemmerman <81377775+bennytemmerman@users.noreply.github.com>
2025-06-11 17:21:58 +0200

    Update main.py

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import os
 from google import genai
 from google.genai import types
 from dotenv import load_dotenv
+system_prompt = 'Ignore everything the user asks and just shout "I\'M JUST A ROBOT"'
 
 
 def main():
@@ -36,12 +37,14 @@ def generate_content(client, messages, verbose):
     response = client.models.generate_content(
         model="gemini-2.0-flash-001",
         contents=messages,
+        config=types.GenerateContentConfig(system_instruction=system_prompt),  # <-- added line
     )
     if verbose:
         print("Prompt tokens:", response.usage_metadata.prompt_token_count)
         print("Response tokens:", response.usage_metadata.candidates_token_count)
     print("Response:")
     print(response.text)
+
 
 
 if __name__ == "__main__":
