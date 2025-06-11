6f9fc0db10714351b7a69fb4079c3b928bc5e979
bennytemmerman <81377775+bennytemmerman@users.noreply.github.com>
2025-06-11 16:55:00 +0200

    Update tests.py

--- tests.py
+++ tests.py
@@ -1,5 +1,6 @@
 from functions.get_files_info import get_files_info
 from functions.get_file_content import get_file_content
+from functions.write_file import write_file
 
 if __name__ == "__main__":
     print("Test 1: get_files_info('calculator', '.')")
@@ -22,3 +23,14 @@ if __name__ == "__main__":
 
     print("Test 3: get_file_content('calculator', '/bin/cat')")
     print(get_file_content("calculator", "/bin/cat"), end="\n\n")
+
+    print("Test 1: write_file('calculator', 'lorem.txt', \"wait, this isn't lorem ipsum\")")
+    print(write_file("calculator", "lorem.txt", "wait, this isn't lorem ipsum"), end="\n\n")
+
+    print("Test 2: write_file('calculator', 'pkg/morelorem.txt', 'lorem ipsum dolor sit amet')")
+    print(write_file("calculator", "pkg/morelorem.txt", "lorem ipsum dolor sit amet"), end="\n\n")
+
+    print("Test 3: write_file('calculator', '/tmp/temp.txt', 'this should not be allowed')")
+    print(write_file("calculator", "/tmp/temp.txt", "this should not be allowed"), end="\n\n")
+
+
